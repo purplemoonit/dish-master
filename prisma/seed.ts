4546708d528213c05911e3c5b873bc10ec3b490c
datokhojava <khojavad30@gmail.com>
2025-07-03 01:56:10 +0400

    integration of prisma with test data and seed script inside, also added docker-compose file

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: 'test@example.com',
+      password: 'hashed_password_here',
+      apiKey: 'sample-api-key-123',
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
